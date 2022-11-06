fe00cd198e737e156e4be4204dfe3a1fe102c415
HebaSalem <puupiiiii@gmail.com>
2022-11-06 20:24:03 +0100

    create products list and its fuctionality

--- js/cateBrand.js
+++ js/cateBrand.js
@@ -1,13 +1,11 @@
 import {displayProducts} from './pagination.js'
 
 export const cateBrandHandler = data => {
- 
-
   
   //Categories
   //get Categories from data
   const allCategories = data.map(product => product.category)
-  const categories = new Set(allCategories.filter (cat => cat != null && cat!= ''))
+  const categories = new Set(allCategories.filter (cat => cat != null && cat != ''))
 
   //create Categories list
   const cateList = document.getElementById('categoryMenu')
@@ -21,7 +19,7 @@ export const cateBrandHandler = data => {
   //Brands
   //get Brands from data
   const allBrands= data.map(product => product.brand)
-  const brands = new Set(allBrands.filter (brand => brand != null && brand!= ''))
+  const brands = new Set(allBrands.filter (brand => brand != null && brand != ''))
 
   //create Brands list
   const brandList = document.getElementById('brandMenu')
@@ -32,55 +30,62 @@ export const cateBrandHandler = data => {
     brandList.appendChild(brandItem)
   })
 
-  //show/hide brands list - hide categories list when open brand list  
+  //ProductTypes
+  //get ProductTypes from data
+  const allProducts = data.map(product => product.product_type)
+  const proTypes = new Set(allProducts.filter (type => type != null && type != ''))
+
+  //create ProductTypes list
+  const productList = document.getElementById('productMenu')
+  proTypes.forEach(type => {
+    const proItem = document.createElement('li')
+    proItem.textContent = type
+    proItem.classList.add('dropdown-item')
+    productList.appendChild(proItem)
+  })
+
+  //show/hide brands list - hide categories, products lists when open brand list  
   document.getElementById('brandId').addEventListener('click' , e => {
     e.stopPropagation()
     brandList.classList.toggle('active')
     cateList.classList.remove('active')
+    productList.classList.remove('active')
   })
 
-  //show/hide categories list - hide brands list when open categories list
+  //show/hide categories list - hide brands, products lists when open categories list
   document.getElementById('catedId').addEventListener('click' , e => {
     e.stopPropagation()
     cateList.classList.toggle('active')
     brandList.classList.remove('active')
+    productList.classList.remove('active')
   })
 
-  //hide categories & brand lists when click on body
+  //show/hide products list - hide brands, categories lists when open categories list
+  document.getElementById('productId').addEventListener('click' , e => {
+    e.stopPropagation()
+    productList.classList.toggle('active')
+    brandList.classList.remove('active')
+    cateList.classList.remove('active')
+  })  
+
+  //hide categories, brand & products lists when click on body
   document.body.addEventListener('click' , ()=> {
     brandList.classList.remove('active')
     cateList.classList.remove('active')
+    productList.classList.remove('active')
   })
 
-  //display products according to clicked brand or category
+
+
+  //display products according to clicked brand or category or productType
   const allItems = Array.from(document.querySelectorAll('.dropdown-item'))
   
   allItems.forEach(item => {
     const itemText = item.textContent
     item.addEventListener('click', ()=> {
-      const filteredData = data.filter(product=> product.category === itemText || product.brand === itemText )
+      const filteredData = data.filter(product=> product.category === itemText || product.brand === itemText || product.product_type === itemText)
       displayProducts(filteredData , pagination , 50 , 0)
     
     })
   })
 }
-
-
-//58 brand
-  //   const filtered = data.map(x => x.brand)
-  //   console.log(new Set(filtered))
-    
-  //10 product_type
-  //   const filtered2 = data.map(x => x.product_type)
-  //   console.log(new Set(filtered2))
-
-  //16 category
-  // const filtered3 = data.map(x => x.category)
-  // console.log(new Set(filtered3))
-
-  //inputField.onfocus = () => console.log('input focus')
-//inputField.onchange = e => console.log(e.target.value)
-/*searchBtn.onclick = e => {
-  e.preventDefault()
-  console.log('search btn clicked');
-}*/
