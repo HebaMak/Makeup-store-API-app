3f24ecf32f251a61c2c2b7910e5baf24c44321fe
HebaSalem <puupiiiii@gmail.com>
2022-11-11 15:54:12 +0100

    edit  js/jq/carosleScript.js file

--- js/jq/carosleScript.js
+++ js/jq/carosleScript.js
@@ -1,39 +1,38 @@
 //ticker
-$(function() {
+$(function () {
   var _scroll = {
     delay: 1000,
-    easing: 'linear',
+    easing: "linear",
     items: 1,
     duration: 0.07,
     timeoutDuration: 0,
-    pauseOnHover: 'immediate'
+    pauseOnHover: "immediate",
   };
-  $('#ticker-1').carouFredSel({
+  $("#ticker-1").carouFredSel({
     width: 1000,
     align: false,
     items: {
-      width: 'variable',
+      width: "variable",
       height: 35,
-      visible: 1
+      visible: 1,
     },
-    scroll: _scroll
+    scroll: _scroll,
   });
-
-  $('#ticker-2').carouFredSel({
+  $("#ticker-2").carouFredSel({
     width: 1000,
     align: false,
     circular: false,
     items: {
-      width: 'variable',
+      width: "variable",
       height: 35,
-      visible: 2
+      visible: 2,
     },
-    scroll: _scroll
+    scroll: _scroll,
   });
 
   //	set carousels to be 100% wide
-  $('.caroufredsel_wrapper').css('width', '100%');
+  $(".caroufredsel_wrapper").css("width", "100%");
 
   //	set a large width on the last DD so the ticker won't show the first item at the end
-  $('#ticker-2 dd:last').width(2000);
+  $("#ticker-2 dd:last").width(2000);
 });
