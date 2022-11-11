759b449a90139ab6822b3f06ea59113a8cf28826
HebaSalem <puupiiiii@gmail.com>
2022-11-11 15:55:37 +0100

    edit js/loadError.js file

--- js/loadError.js
+++ js/loadError.js
@@ -1,23 +1,22 @@
-const pagination = document.getElementById('pagination')
+const pagination = document.getElementById("pagination");
 
-export const errorMsg = err => {
-  const msg = document.createElement('div')
-  msg.classList = 'error_message'
+export const errorMsg = (err) => {
+  const msg = document.createElement("div");
+  msg.classList = "error_message";
   msg.innerHTML = `
   <h1>Sorry, There is an error through Fetching..</h1>
   <h2>${err.message}</h2>
-  `
-  pagination.innerHTML =''
-  pagination.appendChild(msg)
-}
-
+  `;
+  pagination.innerHTML = "";
+  pagination.appendChild(msg);
+};
 
 export const loading = () => {
-  const loadingImg = document.createElement('div')
-  loadingImg.classList = 'loading_img'
+  const loadingImg = document.createElement("div");
+  loadingImg.classList = "loading_img";
   loadingImg.innerHTML = `
     <h4>Hi... Get some tea and come back in ten minutes .. brands will come soon</h4>
     <img src=" https://res.cloudinary.com/hapiii/image/upload/v1648560350/loading%20img/kqwlyn201w8ohsk9te7o.gif" alt="loading img" />
-  `
-  pagination.appendChild(loadingImg)
-}
+  `;
+  pagination.appendChild(loadingImg);
+};
